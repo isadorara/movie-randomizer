eeee88ea5955146632fa4c08913da59bc86151b3
isadorara <isadora_caraujo@outlook.com>
2026-08-27 22:27:48 -0300

    Show movie list grid

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const API_KEY = TMDB_API_KEY(); //env.js
-const URL = 'https://api.themoviedb.org/3/search/movie?query=';
+const API_KEY = 'REMOVED'; //env.js
+const URL = 'https://api.themoviedb.org/3/search/movie?language=pt&query=';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
+const GENRE_URL = `https://api.themoviedb.org/3/genre/movie/list?language=pt&api_key=${API_KEY}`;
 
 let listaFilmes = [];
 
@@ -8,6 +9,16 @@ const inputFilme = document.querySelector(".adicionar-filme input");
 const printFilme = document.querySelector(".adicionar-filme button");
 const escolherFilme = document.querySelector(".escolher-filme");
 const poster = document.querySelector(".container-filme img");
+const container = document.querySelector('.lista-filmes')
+const posterLista = document.querySelector(".elemento-filme img");
+
+//Generos
+const options = {
+    method: 'GET',
+}
+fetch(`https://api.themoviedb.org/3/genre/movie/list?language=pt&api_key=${API_KEY}`, options)
+    .then(res => res.json())
+    .catch(err => console.error(err));
 
 //Chama API
 async function mostrarFilme(filme) {
@@ -27,10 +38,33 @@ async function mostrarFilme(filme) {
         document.querySelector(".error").style.display = "none";
         document.querySelector(".container-filme").style.display = "flex";
     }
-    
 }
 
 //Adiciona filme à lista 
+async function adicionarFilme(filme) {
+    const response = await fetch(URL + filme + `&api_key=${API_KEY}`);
+
+    if(response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+    } else {
+        var data = await response.json();
+
+        var posterLista = IMG_URL + data.results[0].poster_path;
+        var titulo = data.results[0].title;
+        var ano = data.results[0].release_date.slice(0, 4);
+
+        const template = `
+            <div class="elemento-filme">
+                <img src="${posterLista}">
+                <h1 id="titulo-filme">${titulo}</h1>
+                <h2 id="ano">${ano}</h2>
+            </div>
+        `;
+
+        container.insertAdjacentHTML('beforeend', template);
+    }
+}
+
 inputFilme.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
         event.preventDefault();
@@ -41,6 +75,8 @@ inputFilme.addEventListener("keydown", function(event) {
 printFilme.addEventListener("click", function() {
     listaFilmes.push(inputFilme.value);
     console.log(listaFilmes);
+    adicionarFilme(inputFilme.value);
+
     inputFilme.value = '';
 });
 
